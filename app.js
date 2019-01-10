2eefae4dbdc1820db06752fd42074f623f2f0c58
ala <alabenhmouda@gmail.com>
2019-01-10 21:48:50 +0100

    Set up messages show route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 require('dotenv').load();
 const auth = require('./routes/auth');
 const messagesRouter = require('./routes/message');
+const {Message} = require('./models');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/warbler');
 mongoose.connection.on('error', () => console.error('connection error!'))
@@ -12,11 +13,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api/auth/', auth);
-app.use('/users/:userId/messages', messagesRouter);
+app.use('/api/users/:userId/messages', messagesRouter);
 app.get('/', (req, res) => {
     res.json({
         message: "Make a request to /api/auth"
     });
+})
+app.get('/api/messages', (req, res) => {
+    Message
+        .find().sort({createdAt: -1})
+        .populate('userId', {username: true, profileImageUrl: true})
+        .then(msgs => {
+            res.status(200).json(msgs);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
 })
 
 app.listen(process.env.PORT || 8080, () => console.log('App is running'));
